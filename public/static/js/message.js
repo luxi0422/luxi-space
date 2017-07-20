d726fa108595f23362fc9aa0257be66f9bbe2879
luxi <luxi0422@yahoo.com.cn>
2017-07-20 08:29:01 +0800

    改版

--- public/static/js/message.js
+++ public/static/js/message.js
@@ -1,7 +1,7 @@
 //ajax从数据库读取留言数据
 var $dataDom = $(".data");
 var messageTpl = $('#message-tpl').html();
-$.getJSON('http://luxi.space/api/message?limit=10',function(data){
+$.getJSON('http://luxi.space/api/message?page=1&limit=20',function(data){
     for(var i =0; i<data.data.list.length; i++) {
         insertMessage(data.data.list[i]);
     }
@@ -22,13 +22,14 @@ function insertMessage(messageInfo, needAnimate) {
     }
 }
 
-//点击提交时，向数据库传参
+//点击提交
 $("form").submit(function(){
+    var date = new Date();
+    var time = date.getFullYear() + "年" + (date.getMonth() + 1) + "月" + date.getDate() + "日"
     var messageInfo = {
         name: $("form input:first-child").val(),
         message: $("textarea").val()
     };
-
     $.post('http://luxi.space/api/message',messageInfo,function(obj){
         if(obj.code != 200) {
             alert(obj.msg);
@@ -39,3 +40,37 @@ $("form").submit(function(){
     },'json');
     return false;
 });
+
+//点击加载更多
+var pageStart=1;
+var pageLimit=20;
+getData(pageStart,pageLimit);
+$(".wrapper .pages").click(function(){
+    pageStart++;
+    getData(pageStart,pageLimit);
+});
+
+function getData(page,limit) {
+    $.ajax({
+        type: "get",
+        url: "http://luxi.space/api/message?page=" + page + "&limit=" + limit,
+        dataType: "json",
+        success: function (response) {
+            var result = "";
+            var data = response.data.list;
+            for (var i = (page - 1) * limit; i < response.length; i++) {
+                result += '<div class="name">'+data[i].name+'：</div><div class="words">'+data[i].message+'</div><div class="date">'+data[i].gmt_create+'</div>';
+            }
+            $(".data").append(result);
+
+            if ((page * limit) > data.length) {
+                $(".wrapper .pages").hide();
+            } else {
+                $(".wrapper .pages").show();
+            }
+        },
+        error: function (xhr, type) {
+            alert("出错啦");
+        }
+    });
+}
