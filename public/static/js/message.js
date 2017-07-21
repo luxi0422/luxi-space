beaca589dbf7051bc082c72f7f27fbd831e10429
luxi <luxi0422@yahoo.com.cn>
2017-07-21 08:38:07 +0800

    初稿

--- public/static/js/message.js
+++ public/static/js/message.js
@@ -42,35 +42,72 @@ $("form").submit(function(){
 });
 
 //点击加载更多
-var pageStart=1;
-var pageLimit=20;
-getData(pageStart,pageLimit);
-$(".wrapper .pages").click(function(){
-    pageStart++;
-    getData(pageStart,pageLimit);
+    var pageStart=1;
+    var pageLimit=20;
+    //getData(pageStart,pageLimit);
+    $(".wrapper .pages").click(function(){
+        pageStart ++;
+        getData(pageStart,pageLimit);
+    });
+
+    function getData(page,limit) {
+        $.ajax({
+            type: "get",
+            url: "http://luxi.space/api/message?page=" + page + "&limit=" + limit,
+            dataType: "json",
+            success: function (response) {
+                var result = "";
+                var data = response.data.list;
+                console.log(page,limit);
+                for (var i = 0; i < data.length; i++) {
+                    result += '<div class="message"><div class="name">'+data[i].name+'：</div><div class="words">'+data[i].message+'</div><div class="date">'+data[i].gmt_create+'</div></div>';
+                }
+                $(".data").append(result);
+
+                if ((page * limit) > response.data.count) {
+                    $(".wrapper .pages").hide();
+                } else {
+                    $(".wrapper .pages").show();
+                }
+            },
+            error: function (xhr, type) {
+                alert("出错啦");
+            }
+        });
+    }
+
+    //回到顶端【还有重复执行的BUG】
+var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+$(".top").mouseenter(
+    function(){
+        $(".top").stop().fadeOut(500);
+        $(".top-z").stop().css("display","block");
+    }
+);
+$(".top-z").mouseleave(
+     function(){
+         $(".top").stop().fadeIn(500);
+         $(".top-z").stop().css("display","none");
+     }
+).click(function(){
+    $('html,body').animate({scrollTop:0},1000);
 });
 
-function getData(page,limit) {
-    $.ajax({
-        type: "get",
-        url: "http://luxi.space/api/message?page=" + page + "&limit=" + limit,
-        dataType: "json",
-        success: function (response) {
-            var result = "";
-            var data = response.data.list;
-            for (var i = (page - 1) * limit; i < response.length; i++) {
-                result += '<div class="name">'+data[i].name+'：</div><div class="words">'+data[i].message+'</div><div class="date">'+data[i].gmt_create+'</div>';
-            }
-            $(".data").append(result);
-
-            if ((page * limit) > data.length) {
-                $(".wrapper .pages").hide();
-            } else {
-                $(".wrapper .pages").show();
-            }
-        },
-        error: function (xhr, type) {
-            alert("出错啦");
-        }
-    });
-}
+//登录
+$(".login").mouseenter(
+    function(){
+        $(".login").stop().fadeOut(500);
+        $(".login-z").stop().css("display","block");
+    }
+);
+$(".login-z").mouseleave(
+    function(){
+        $(".login").stop().fadeIn(500);
+        $(".login-z").stop().css("display","none");
+    }
+).click(function(){
+    $(".adminLogin").toggle();
+});
+$(".adminLogin div").click(function(){
+    $(".adminLogin").css("display","none");
+});
