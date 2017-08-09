a0fc8fd2b1151b09b16827a73158a84b22cb011d
王钟凯 <zhongkai.wzk@alibaba-inc.com>
2017-08-09 15:34:12 +0800

    登录

--- public/static/js/message.js
+++ public/static/js/message.js
@@ -110,6 +110,9 @@ $(".data").delegate(".message .del","click",function(data){
     console.log(this);
     console.log(this.dataset.message);
     var that = this;
+    if(isLogin) {
+        $(this).show();
+    }
     $.ajax({
         type:"delete",
         url:"http://luxi.space/api/message",
@@ -120,4 +123,9 @@ $(".data").delegate(".message .del","click",function(data){
             $(that).parent().remove();
         }
     })
+});
+
+var isLogin = false;
+checkLogin(function(){
+  isLogin = true;
 });
